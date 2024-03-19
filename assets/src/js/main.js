425c4a8b7d51a20516e2db007ad7f317c414c8a5
iftekher2108 <parvezkhan21082002@gmail.com>
2024-03-19 09:42:39 +0600

    mejor ui change

---
+++ assets/src/js/main.js
@@ -0,0 +1,22 @@
+console.log('iftekher mahmud pervez')
+
+
+$('.offer-slider').slick({
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 500,
+  centerMode: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+})
+
+
+$('.hero-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
