b3c143aee2504fcf54d459497485880272a692bc
iftekher2108 <parvezkhan21082002@gmail.com>
2024-03-23 14:00:09 +0600

    all complete

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,5 +1,16 @@
 console.log('iftekher mahmud pervez')
 
+// wow initialization
+AOS.init({
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  offset: 250, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
+  easing: 'ease-in-out', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+})
+
 // banner offer slide
 $('.offer-slider').slick({
   dots: false,
@@ -32,3 +43,11 @@ $('.hero-slider').slick({
     // console.log($(this).attr('title'))
 
   })
+
+
+
+  // loader scripts
+  $(document).ready(function(){
+    $('#loader').fadeOut(300);
+  })
+
